c3b577e0df534fac07df8625bc9ccd3afaa32c9c
Jojo-PC <joeycw328@gmail.com>
2015-01-11 01:00:37 -0800

    added helpers for setting and getting particle attributes

--- pyqgfield/items/GLParticleItem.py
+++ pyqgfield/items/GLParticleItem.py
@@ -6,28 +6,42 @@ class GLParticleItem(GLMeshItem):
         """
         ============== =====================================================
         Arguments
-        meshdata       MeshData object from which to determine geometry for 
-                       this item.
-        color          Default face color used if no vertex or face colors 
-                       are specified.
-        edgeColor      Default edge color to use if no edge colors are
-                       specified in the mesh data.
-        drawEdges      If True, a wireframe mesh will be drawn. 
-                       (default=False)
-        drawFaces      If True, mesh faces are drawn. (default=True)
-        shader         Name of shader program to use when drawing faces.
-                       (None for no shader)
-        smooth         If True, normal vectors are computed for each vertex
-                       and interpolated within each face.
-        computeNormals If False, then computation of normal vectors is 
-                       disabled. This can provide a performance boost for 
-                       meshes that do not make use of normals.
+        charge          this particle's electric charge  
+        location        numpy.array([x,y,z]) indicating the x,y,z coordinates of this 
+                        instance in the parent item's coordinate system
         ============== =====================================================
         """
+        self.opts={
+            'charge':0.
+            'location':array([0.,0.,0.])
+        }
         GLMeshItem.__init__(
             self,
-            meshdata=MeshData.sphere(rows=10, cols=20, radius=.25),
-            color=(0,0,1,.75),
+            meshdata=MeshData.sphere(rows=10, cols=20, radius=1),
+            color=(1.,1.,1.,.75),
             shader='shaded'
             )
+        self.setData(**kwds)
         
+    #public function for setting particle attributes
+    def setData(self, **kwds):
+        if 'charge' in kwds and (kwds['charge'] not self.opts['charge']):
+                self._setCharge(kwds['charge'])
+        if 'location' in kwds and (kwds['location'] not self.opts['location']):
+            self._setLocation(kwds['location'])
+            
+            
+    #helper function for updating particle charge
+    def _setCharge(self, charge):
+        self.opts['charge']=charge
+    
+    #helper function for updating particle location
+    def _setLocation(self, location)
+        cur=self.opts['location']
+        [dx,dy,dz]=[location[0]-cur[0],location[1]-cur[1], location[2]-cur[2]]
+        self.translate(dx,dy,dz,local=False)
+        
+    #public function for retrieving particle attributes
+    def get(self, opt):
+        if opt in self.opts:
+            return(self.opts[opt])
